d7cf64b3c31dcb3d2303a6c9bc1132d763036f7b
catalistt <64863044+catalistt@users.noreply.github.com>
2020-06-26 13:12:11 -0400

    Update Desafio4.sql

--- Desafio4.sql
+++ Desafio4.sql
@@ -1,20 +1,50 @@
 CREATE TABLE Libro(
 		codigo INT,
 		titulo VARCHAR(50) NOT NULL,
-		autor VARCHAR(50) NOT NULL,
-		editorial VARCHAR(50) NOT NULL,
 		PRIMARY KEY(codigo));
 
---NO SE CREA UNA TABLA DE LECTOR, PORQUE NO HAY MÁS ATRIBUTOS QUE EL NOMBRE
---NO SE ASUMEN DATOS QUE NO APARECEN EN LA TABLA (COMO PODRÍA SER "FECHA DE PRÉSTAMO")
+CREATE TABLE Autor(
+		id_autor SERIAL,
+		Nombre_autor VARCHAR(50) NOT NULL,
+		PRIMARY KEY(id_autor));
 
-CREATE TABLE Arriendo(
-		id_arriendo SERIAL,
-		codigo_libro INT NOT NULL,
+CREATE TABLE Lector(
+		id_lector SERIAL,
+		Nombre_1 VARCHAR(30) NOT NULL,
+		Nombre_2 VARCHAR(30),
+		Apellido_1 VARCHAR(30),
+		Apellido_2 VARCHAR(30),
+		PRIMARY KEY(id_lector));
+
+CREATE TABLE Editorial(
+		id_editorial SERIAL,
+		Nombre VARCHAR(30) NOT NULL,
+		PRIMARY KEY(id_editorial));
+
+CREATE TABLE Arrendado_por(
+		codigo_libro INT,
+		id_lector INT,
 		fecha_devolucion DATE NOT NULL,
-		nombre_lector VARCHAR(50) NOT NULL,
-		PRIMARY KEY(id_arriendo),
+		PRIMARY KEY(codigo_libro, id_lector),
 		FOREIGN KEY(codigo_libro) REFERENCES 
-		Libro(codigo));
+		Libro(codigo),
+		FOREIGN KEY(id_lector) REFERENCES
+		Lector(id_lector));
 
-		
+CREATE TABLE Es_editado(
+		Libro INT,
+		Editorial INT,
+		PRIMARY KEY (Libro, Editorial),
+		FOREIGN KEY (Libro) REFERENCES
+		Libro(codigo),
+		FOREIGN KEY (Editorial) REFERENCES
+		Editorial(id_editorial));		
+
+CREATE TABLE Es_escrito(
+		Codigo_libro INT,
+		Id_autor INT,
+		PRIMARY KEY(Codigo_libro, Id_autor),
+		FOREIGN KEY (Codigo_libro) REFERENCES
+		Libro(codigo),
+		FOREIGN KEY (Id_autor) REFERENCES
+		Autor(id_autor));
